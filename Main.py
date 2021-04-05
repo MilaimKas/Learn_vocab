9d210e16bb03e6d1b6427ef64fbe988d4bd698a2
milaim <milaim.kas@gmail.com>
2021-04-05 19:14:54 +0200

    - second commit

--- Main.py
+++ Main.py
@@ -1,16 +1,48 @@
 import WS_class
 import random
+import sys
 
+# stop command
+exit = ['exit','stop','quit']
+# list of keys
+keys = ["translation","synonyme","article","theme"]
+
+# initializing
 print('Initialization, wait a few seconds ...')
-word_list = WS_class.WS()
-var = input(print("Do you want to test your skills (type 1) or add new words (type 2) "))
+WS = WS_class.WS()
+print("Do you want to test your skills (type 1) or add new words (type 2) ")
+var = input()
 
 # Exercise
-if var == 1:
+if var == '1':
    # random test F --> D
-   print(random.choice(word_list))
+   print('press enter to start')
+   while input() not in exit:
+       word = WS.print_rand()
+       print('Translate the word: ',word["word"])
+       if input() in word[keys[0]]:
+           print('Correct !')
+       else:
+           print('Incorrect, last chance')
+           if input() in word[keys[0]]:
+               print('Correct !')
+           else:
+               print('Incorrect')
+               print('The correct translation is: ', word[keys[0]])
+
    # random test D --> F
 
 # Add new words
-if var == 2:
+if var == '2':
+    tmp = []
+    print('Word to add')
+    w = input()
+    for key in keys:
+        print(key)
+        tmp.append((key,input()))
+
+    WS.add_word(w,tmp)
+
+else:
+   print("Do you want to test your skills (type 1) or add new words (type 2) ")
 
--- WS_class.py
+++ WS_class.py
@@ -1,29 +1,43 @@
 import random
+import json
 
 # keys
-aa = "word"
-a  = "Theme"
-b  = "artikel"
-c  = "Übersetzung französich"
-d  = "Synonyme"
-e  = "Beispiel"
-f  = "plural"
+w = "word"
+t  = "Theme"
+a  = "artikel"
+ub  = "Übersetzung französich"
+s  = "Synonyme"
+ex  = "Beispiel"
+plu  = "plural"
 
 # WS List class
 class WS():
     def __init__(self):
         '''
         List of words
-        --> my_list: list of dictionary variables containing keys
+        --> my_list.txt: list of dictionary variables containing keys
         --> extra_list: simple list of words (with article)
         '''
 
-        # initialize list of words found in my_list
+        # initialize list of words found in my_list.txt
         self.my_list = []
-        with open('my_list') as f:
-            self.my_list.append([line.rstrip() for line in f])
+        with open('my_list.txt', 'r') as file:
+            Lines = file.readlines()
+            for line in Lines:
+                self.my_list.append(json.loads(line.strip()))
 
         self.extra_list = []
+
+    def init_mylist(self):
+        """
+        re-initialize my_list (if new words have been added for example)
+        :return:
+        """
+        self.my_list = []
+        with open('my_list.txt', 'r') as file:
+            Lines = file.readlines()
+            for line in Lines:
+                self.my_list.append(json.loads(line.strip()))
 
     def add_extra_list(self,file):
         '''
@@ -32,37 +46,50 @@ class WS():
         '''
 
         with open(file) as f:
-            self.extra_list.append([line.rstrip() for line in f])
+            self.extra_list.extend([line.rstrip() for line in f])
 
 
-    def add_word(self,word,**kwargs):
+    def add_word(self,word,item=None):
         '''
-        Update my_list with a new word and related keys,
+        Update my_list.txt with a new word and related keys,
         or add one or more additional keys to an already stored word
 
         :param word: word to had to the list
-        :param kwargs: additional, optional, keys
+        :param kwargs: additional, optional, keys and item (must be a list of tuple)
         '''
 
         # check if word is already stored
-
-        # if so, update keys and values
-        # if not append my_list
-
-        # Updates my_list in my_list.txt file
-
+        for stored_word in self.my_list:
+            if word in stored_word["word"]:
+                print('This word is already stored with the following related keys: ')
+                print(stored_word)
+                #print('Do you wish to add a new key (yes/no) ?')
+                #if input() == 'yes':
+            # write new line in my_list.txt
+            else:
+                new_word = '{'+'\"word\": \"{}\"'.format(word)
+                if item is not None:
+                    for key, it in item:
+                        new_word += ', '+'\"{}\": \"{}\"'.format(key, it)
+                    new_word += '}'
+                    with open('my_list.txt', 'a') as file:
+                        file.write(str(new_word)+'\n')
+                break
 
     def print_rand(self):
+        # todo: different format between my_list and extra_list
         '''
-        Print random word in german and asks for translation
+        Print random word in german (or french) and asks for translation
         '''
 
-        tot_list = self.my_list.append(self.extra_list)
-        print(random.choice(tot_list)['word'])
+        tot_list = self.my_list+self.extra_list
+
+        return random.choice(tot_list)
+
 
 if __name__ == '__main__':
 
     WS = WS()
     print(len(WS.my_list[0]))
     print(WS.my_list)
-    #WS.print_rand()
+    WS.add_word("leichtsinnig", [('translation','inconsidéré'), ('synonyme', 'fahrlässig')])
