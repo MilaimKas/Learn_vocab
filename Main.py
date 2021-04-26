1ac09c0377a6727d64c5ac5371f2803a15522119
milaim <milaim.kas@gmail.com>
2021-04-26 21:43:59 +0200

    - Adding add word - Adding show list - List format for my_list file

--- Main.py
+++ Main.py
@@ -1,72 +1,137 @@
 '''
 
 todo: make display list for add new word option
+todo: initialze my_list file if it does not exist
 todo: Make my_list a table with column corresponding to keys, with WS class creating list of dict
 todo: google translate option
 todo:
 
 '''
 
-import WS_class
+import VOC_class
 import PySimpleGUI as sg
+from tabulate import tabulate
 
-# stop command
-exit = ['exit','stop','quit']
-
+##########################################
 # initializing vocabulary list and class
-#print('Initialization, wait a few seconds ...')
-WS = WS_class.WS()
-keys = WS.keys
-#print("Do you want to test your skills (type 1) or add new words (type 2) ")
-#var = input()
-var = None
+##########################################
 
+WS = VOC_class.VOC()
+keys = WS.keys
+
+############################
 # initializing GUI window
+############################
+
 sg.theme('DarkAmber')   # Add a touch of color
+
 # ----- Full layout -----
+new_word_layout = []
+for k in keys:
+    new_word_layout.append([sg.Text(k+' :', size=(10,1)), sg.InputText(key=k)])
+
 layout = [
-        [sg.Text("TEST YOUR SKILLS")],
+        [sg.Text("TEST YOUR SKILLS", size=(70,1), justification='center', relief=sg.RELIEF_RIDGE)],
         [sg.Button("Generate word", key="-generate-"), sg.In(enable_events=True, key="-word_to_translate-")],
-        [sg.Text("Enter translation: "), sg.InputText(), sg.Button('Check', key="-Check-")],
+        [sg.Text("Enter translation: "), sg.InputText(), sg.Button('Check', key="-check-")],
+        [sg.Text('', key='-answer-')],
+        [sg.Text('')],
         [sg.HSeparator()],
-        [sg.Text("ADD A NEW WORD TO YOUR PERSONAL LIST")],
-        [sg.Text("New word: ", key="-displayed_key-"), sg.InputText(key="-new_word-", enable_events=True)],
-        [sg.Button('ADD', key="-add_word-")],
+        [sg.Text('')],
+        [sg.Text("ADD A NEW WORD TO YOUR PERSONAL LIST", size=(70,1), justification='center',relief=sg.RELIEF_RIDGE)],
+        [sg.Frame(title="New word to add", layout=new_word_layout), sg.Button('ADD', key="-add_word-")],
+        [sg.Text('')],
+        [sg.HSeparator()],
+        [sg.Text('')],
+        [sg.Text("DISPLAY YOUR PERSONAL LIST", size=(70,1), justification='center',relief=sg.RELIEF_RIDGE)],
+        [sg.Button('SHOW LIST', enable_events=True, key='-show_list-')],
+        [sg.Text('')],
+        [sg.HSeparator()],
+        [sg.Text('')],
         [sg.Cancel("Exit")],
 ]
 # Create the window
-window = sg.Window("Deutshes Wortschatz", layout)
+window = sg.Window("Build vocabulary list", layout)
 
 word = None
+
+#################
 # Event loop
+#################
+
 while True:
 
     # read action on the window
     event, values = window.read()
 
-    # ---- check event ----
+    # remove printed message
+    window['-answer-'].update('')
 
-    # Translate
+    #  Translate
+    # ----------------------------
 
     if event == "-generate-":
         # print random word present in WS.list
         word = WS.print_rand()
         window["-word_to_translate-"].update(word[keys[0]])
 
-    if event == "-Check-":
+    if event == "-check-":
         if word is None:
             sg.popup("You have to generate a word to translate before given a translation !!")
         else:
-            if values[0] in word[keys[3]]:
-                sg.popup('Correct !')
+            # check if translation is correct
+            if values[0] in word[keys[1]]:
+                # print message bellow
+                window['-answer-'].update('Correct !')
+            # check if synonym
+            elif values[0] in word[keys[7]]:
+                # print messsage bellow
+                window['-answer-'].update('Correct but a better translation would be {}'.format(word[keys[1]]))
             else:
-                text_out = word[keys[3]]
-                #text_out = ''
-                #for i in word:
-                #    text_out += '{}, -> {} \n'.format(i, word[i])
-                sg.popup('Incorrect, the translation is: \n'+text_out)
+                # print correct answer
+                text_out = word[keys[1]]+word[keys[7]]
+                window['-answer-'].update('Incorrect, the translation is/are: '+text_out)
 
-    # Add word
+    # Add word to my_list
+    # -----------------------
+
+    if event == "-add_word-":
+
+        # if no word is given
+        if len(window[keys[0]].get()) == 0:
+            sg.popup('At least a new word must be given in order to add a new item to your list !')
+
+        else:
+            new_word = []
+            for k in keys:
+                if len(window[k].get()) != 0:
+                    new_word.append(window[k].get())
+                else:
+                    new_word.append(" ")
+
+            # Create new window for confirmation
+            new_window = sg.Window('Add word',
+                               [
+                                [sg.Text('Add the following new word to your list ?')],
+                                [sg.Text(str(new_word))],
+                                [sg.Button('YES', key='-yes-'), sg.Button('NO', key='-no-')]
+                               ])
+            # read event in new window
+            while True:
+                # todo: yes and no events do not work
+                ev, val = new_window.read()
+                if ev == sg.WIN_CLOSED:
+                    break
+                if ev == '-yes-':
+                    WS.add_word(new_word)
+                if ev == '-no-':
+                    break
+
+    # -- Show list --
+    if event == "-show_list-":
+        # todo: sort my_list alphabetically
+        table = tabulate(WS.my_list, headers=keys, tablefmt='rst')
+        sg.Window('My list of vocabulary in alphabetical order', [[sg.Text(table, font='Courier')]]).read()
 
     # close window
     if event == sg.WIN_CLOSED or event == "Exit":
---
+++ VOC_class.py
@@ -0,0 +1,144 @@
+import random
+import json
+import re
+
+field_sep = "[,:;\t]"
+
+# WS List class
+class VOC():
+    def __init__(self, fout="my_list_example.txt"):
+        '''
+        List of words
+        --> my_list_example.txt: list of words with relevant fields store in 6th line
+        '''
+
+        self.fout = fout
+
+        # my_list in dictionary format
+        self.my_dict = []
+        # my_list in list format
+        self.my_list = []
+
+        # initialize list
+        # ------------------------------------------
+
+        with open(self.fout, 'r') as file:
+
+            Lines = file.readlines()
+            tmp_str = Lines[6][2:-1] # remove \n
+
+            # store keys
+            self.keys = re.split(field_sep,tmp_str)
+
+            # store words
+            for line in Lines[8:]:
+
+                # initialize dict for my_dict
+                word = {}
+                line = re.split(field_sep, line)
+
+                # remove new line character /n
+                line = line[:-1]
+
+                # store word in my_list
+                self.my_list.append(line)
+
+                # check for format issue
+                if not line: # look for empty line
+                    break
+                if len(line) != len(self.keys): # look for format issue at specific line
+                    raise ValueError('The format is incorrect for the line containing the word {}'.format(line[0]))
+
+                for key, field in zip(self.keys, line):
+                    word[key] = field
+                self.my_dict.append(word)
+        del tmp_str
+
+        self.extra_key = []
+        self.extra_list = []
+
+    def add_extra_list(self, f_extra, key):
+        '''
+        add additional list of words from file with given keys
+        :return:
+        '''
+
+        # store new key related to additional list
+        self.extra_key.append(key)
+
+        # store words in tmp_list
+        tmp_list = []
+        with open(f_extra, 'r') as file:
+
+            lines = file.readlines()
+
+            # store words
+            for line in lines:
+
+                word = {}
+                line = re.split(field_sep, line)
+
+                # check for format issue
+                if len(line) != len(key):
+                    raise ValueError('The format is incorrect for the line containing the word {}'.format(line[0]))
+
+                for k,field in zip(key, line):
+                    word[k] = field
+                tmp_list.append(word)
+
+        # add new list to extra_list
+        self.extra_list.append(tmp_list)
+        del tmp_list
+
+
+    def add_word(self, add_word):
+        '''
+        Update my_list_example.txt with a new word and related keys,
+        or add one or more additional keys to an already stored word
+
+        :param add_word: list of keys to add to my_list.
+                         [word, translation, etc]
+        '''
+
+        # check if word is already stored
+        # --------------------------------
+
+        stored = False
+        for stored_word in self.my_dict:
+            if add_word[0] in stored_word[self.keys[0]]:
+                stored = True
+                break
+
+        if not stored:
+
+            # print new word in my_list_example.txt
+            with open(self.fout, 'a') as file:
+                file.write(','.join(add_word)+'\n')
+
+            # append my_list
+            self.my_list.append(add_word)
+
+            # append my_dict
+            word = {}
+            for k,e in zip(self.keys, add_word):
+                word[k] = e
+            self.my_dict.append(word)
+
+    def print_rand(self):
+        '''
+        Print random word and ask for translation
+        '''
+
+        tot_list = self.my_dict + self.extra_list
+
+        return random.choice(tot_list)
+
+
+if __name__ == '__main__':
+
+    WS = VOC()
+    print(len(WS.my_dict[0]))
+    print(WS.my_dict)
+    WS.add_word(["leichtsinnig", "inconsidéré", "adjectiv", "", "", "", "", ""])
+    print(WS.my_dict[-1])
+    print(WS.my_list[-1])
